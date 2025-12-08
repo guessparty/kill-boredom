391df4a4d02b897559d395205e108c1f951b4e2a
Nicolas <boitardn@icloud.com>
2025-12-08 14:34:56 +0000

    Ajout de nouveaux jeux

--- src/data.jsx
+++ src/data.jsx
@@ -1,4 +1,4 @@
-import { Gamepad2, Ghost, Zap } from 'lucide-react';
+import { Gamepad2, Ghost, Zap, Citrus, ArrowBigUpDash, Candy } from 'lucide-react';
 
 // Liste des sites inutiles pour le bouton "Je m'ennuie"
 export const externalSites = [
@@ -33,5 +33,30 @@ export const games = [
     description: "Détente absolue.",
     icon: <Gamepad2 size={40} color="#34d399" />,
     url: "https://html5.gamemonetize.co/yhgdcw5brwugmqftpt96krmliugwgcxe/"
-  }
+  },
+  {
+    id: 4,
+    title: "Ninja Fruit",
+    category: "Fun",
+    description: "Des fruits et encore des fruits",
+    icon: <Citrus size={40} color="#eab308" />,
+    url: "https://html5.gamemonetize.co/4kci7og3klgj0ivy2wz3gdvd9dth5e7n/"
+  },
+{
+    id: 5,
+    title: "Mario Run",
+    category: "Vitesse",
+    description: "Cours Mario ! Cours !",
+    icon: <ArrowBigUpDash size={40} color="#3b82f6" />,
+    url: "https://html5.gamemonetize.co/pooszvaulpq40v81v8z3cfdpzau4hro6/"
+  },
+{
+    id: 6,
+    title: "Candy Shuffle",
+    category: "Puzzle",
+    description: "Fais exploser les bonbons !",
+    icon: <Candy size={40} color="#a78bfa" />,
+    url: "https://html5.gamemonetize.co/b7j6efxqew47djofhkag395vb1ijn3ny/"
+  },
+  
 ];
